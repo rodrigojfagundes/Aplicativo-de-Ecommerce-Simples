5cdbd8309186f8ab9124b1b5045585fa7e38c354
rodrigojfagundes <rodrigofgds@hotmail.com>
2021-09-07 21:25:34 -0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/cart.dart';
+import 'package:shop/models/order_list.dart';
+import 'package:shop/models/product_list.dart';
+import 'package:shop/pages/cart_page.dart';
+import 'package:shop/pages/orders_page.dart';
+import 'package:shop/pages/product_detail_page.dart';
+import 'package:shop/pages/products_overview_page.dart';
+import 'package:shop/utils/app_routes.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => ProductList(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => Cart(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => OrderList(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato',
+        ),
+        // home: ProductsOverviewPage(),
+        routes: {
+          AppRoutes.HOME: (ctx) => ProductsOverviewPage(),
+          AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailPage(),
+          AppRoutes.CART: (ctx) => CartPage(),
+          AppRoutes.ORDERS: (ctx) => OrdersPage(),
+        },
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+  }
+}
